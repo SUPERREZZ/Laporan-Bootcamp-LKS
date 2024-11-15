ff747a7b01abda2e6f12af00a4b2bf643ee4ee1f
SUPERREZZ <rezaghiyatsfikri@gmail.com>
2024-11-15 09:58:38 +0700

    done

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace laporan1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.datagridview = new System.Windows.Forms.DataGridView();
+            this.namatb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sekolahcb = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.kotacb = new System.Windows.Forms.ComboBox();
+            this.Simpanbtn = new System.Windows.Forms.Button();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nama = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sekolah = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // datagridview
+            // 
+            this.datagridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagridview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Nama,
+            this.Sekolah});
+            this.datagridview.Location = new System.Drawing.Point(32, 53);
+            this.datagridview.Name = "datagridview";
+            this.datagridview.Size = new System.Drawing.Size(741, 198);
+            this.datagridview.TabIndex = 0;
+            this.datagridview.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.table_CellContentClick);
+            // 
+            // namatb
+            // 
+            this.namatb.Location = new System.Drawing.Point(121, 291);
+            this.namatb.Name = "namatb";
+            this.namatb.Size = new System.Drawing.Size(201, 20);
+            this.namatb.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nama";
+            // 
+            // sekolahcb
+            // 
+            this.sekolahcb.FormattingEnabled = true;
+            this.sekolahcb.Location = new System.Drawing.Point(121, 342);
+            this.sekolahcb.Name = "sekolahcb";
+            this.sekolahcb.Size = new System.Drawing.Size(201, 21);
+            this.sekolahcb.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sekolah";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 394);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kota";
+            // 
+            // kotacb
+            // 
+            this.kotacb.FormattingEnabled = true;
+            this.kotacb.Location = new System.Drawing.Point(121, 396);
+            this.kotacb.Name = "kotacb";
+            this.kotacb.Size = new System.Drawing.Size(201, 21);
+            this.kotacb.TabIndex = 5;
+            // 
+            // Simpanbtn
+            // 
+            this.Simpanbtn.Location = new System.Drawing.Point(395, 342);
+            this.Simpanbtn.Name = "Simpanbtn";
+            this.Simpanbtn.Size = new System.Drawing.Size(102, 23);
+            this.Simpanbtn.TabIndex = 7;
+            this.Simpanbtn.Text = "Simpan";
+            this.Simpanbtn.UseVisualStyleBackColor = true;
+            this.Simpanbtn.Click += new System.EventHandler(this.Simpanbtn_Click);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "ID";
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.Visible = false;
+            // 
+            // Nama
+            // 
+            this.Nama.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Nama.DataPropertyName = "Nama";
+            this.Nama.HeaderText = "Nama";
+            this.Nama.Name = "Nama";
+            // 
+            // Sekolah
+            // 
+            this.Sekolah.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Sekolah.DataPropertyName = "Sekolah";
+            this.Sekolah.HeaderText = "Sekolah";
+            this.Sekolah.Name = "Sekolah";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Simpanbtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.kotacb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sekolahcb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.namatb);
+            this.Controls.Add(this.datagridview);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.datagridview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView datagridview;
+        private System.Windows.Forms.TextBox namatb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox sekolahcb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox kotacb;
+        private System.Windows.Forms.Button Simpanbtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nama;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sekolah;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laporan1
+{
+    public partial class Form1 : Form
+    {
+        SekolahDBEntities db = new SekolahDBEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            load();
+            loadButton();
+        }
+
+        private void table_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == datagridview.Columns["btnUbah"].Index)
+            {
+                var selectedRow = datagridview.Rows[e.RowIndex];
+                int id = (int) selectedRow.Cells["ID"].Value;
+                var data = db.Students.FirstOrDefault(d => d.ID == id);
+                if (data != null)
+                {
+                    namatb.Text = data.Nama;
+                    sekolahcb.SelectedValue = data.SekolahID;
+                    kotacb.SelectedValue = data.KotaID;
+                }
+
+            }
+            if (e.RowIndex >= 0 && e.ColumnIndex == datagridview.Columns["btnHapus"].Index)
+            {
+                var selectedRow = datagridview.Rows[e.RowIndex];
+                int id = (int)selectedRow.Cells["ID"].Value;
+                var data = db.Students.FirstOrDefault(d => d.ID == id);
+                if (data != null)
+                {
+                    DialogResult result = MessageBox.Show(
+                        $"Apakah Anda yakin ingin menghapus data siswa dengan nama {data.Nama} ?", 
+                        "Konfirmasi",                            
+                        MessageBoxButtons.YesNo,                  
+                        MessageBoxIcon.Question                   
+);
+                    db.Students.Remove(data);
+                    db.SaveChanges();
+                    load();
+                }
+            }
+        }
+        private void load()
+        {
+            sekolahcb.DataSource = db.Sekolahs.ToList<Sekolah>();
+            sekolahcb.DisplayMember = "Nama";
+            sekolahcb.ValueMember = "ID";
+            sekolahcb.SelectedIndex = -1;
+
+            kotacb.DataSource = db.Kotas.ToList<Kota>();
+            kotacb.DisplayMember = "Nama";
+            kotacb.ValueMember = "ID";
+            kotacb.SelectedIndex = -1;
+
+            datagridview.DataSource = db.Students.ToList<Student>().Select(n => new 
+            {
+                ID = n.ID,
+                Nama = n.Nama,
+                Sekolah = n.Sekolah.Nama,
+                Kota = n.Kota.Nama,
+            }).ToList();
+            
+        }
+        
+        private void loadButton()
+        {
+            DataGridViewButtonColumn btnUbah = new DataGridViewButtonColumn();
+            btnUbah.HeaderText = "Edit";
+            btnUbah.Name = "btnUbah";
+            btnUbah.Text = "Ubah";
+            btnUbah.UseColumnTextForButtonValue = true;
+            datagridview.Columns.Add(btnUbah);
+
+
+            DataGridViewButtonColumn btnHapus = new DataGridViewButtonColumn();
+            btnHapus.HeaderText = "Hapus";
+            btnHapus.Name = "btnHapus";
+            btnHapus.Text = "Hapus";
+            btnHapus.UseColumnTextForButtonValue = true;
+            datagridview.Columns.Add(btnHapus);
+        }
+
+        private void Simpanbtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(namatb.Text) && sekolahcb.Items != null && kotacb.Items != null)
+            {
+                var check = db.Students.FirstOrDefault(s => s.Nama == namatb.Text);
+                if (check == null)
+                {
+                    Student student = new Student
+                    {
+                        Nama = namatb.Text,
+                        KotaID = Convert.ToInt32(kotacb.SelectedValue),
+                        SekolahID = Convert.ToInt32(sekolahcb.SelectedValue)
+                    };
+                    db.Students.Add(student);
+                }
+                else 
+                {
+                    check.Nama = namatb.Text;
+                    check.KotaID = Convert.ToInt32(kotacb.SelectedValue);
+                    check.SekolahID = Convert.ToInt32(sekolahcb.SelectedValue);
+                }
+                db.SaveChanges();
+            }
+            load();
+        }
+    }
+}
